250ce6d60c455cae8729f6feddca4d461b7b0c87
nicolao00 <nicolao00@naver.com>
2024-08-03 01:03:41 +0900

    feat: 에러 핸들러 설정

--- src/main/java/com/example/upload/domain/type/ErrorCode.java
+++ src/main/java/com/example/upload/domain/type/ErrorCode.java
@@ -8,7 +8,8 @@ import org.springframework.http.HttpStatus;
 @RequiredArgsConstructor
 public enum ErrorCode {
     NOT_FOUND("404", HttpStatus.NOT_FOUND, "Not Exist"),
-    NOT_END_POINT("400", HttpStatus.BAD_REQUEST, "Not Exist End Point Error");
+    NOT_END_POINT("400", HttpStatus.BAD_REQUEST, "Not Exist End Point Error"),
+    SERVER_ERROR("5000", HttpStatus.INTERNAL_SERVER_ERROR, "Internal Server Error");
 
     private final String code;
     private final HttpStatus httpStatus;
